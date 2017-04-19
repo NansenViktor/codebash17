ceba5e10c7580c57800d8eadbe27f48db103d57d
Viktor Gars <viktor.gars@nansen.se>
2017-04-19 18:59:55 +0200

    Work

--- CodeBash2017/CodeBash2017/Models/Pages/StartPage.cs
+++ CodeBash2017/CodeBash2017/Models/Pages/StartPage.cs
@@ -1,10 +1,13 @@
 ﻿using EPiServer.Core;
 using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodeBash2017.Models.Pages
 {
-    [ContentType(DisplayName = "StartPage", GUID = "b8fe8485-587d-4880-b485-a52430ea55de", Description = "Landing page for the website.")]
-    public class StartPage : PageData
-    {
-    }
+	[ContentType(DisplayName = "StartPage", GUID = "b8fe8485-587d-4880-b485-a52430ea55de", Description = "Landing page for the website.")]
+	public class StartPage : PageData
+	{
+		[Display(Name = "Top List")]
+		public virtual string TopList { get; set; }
+	}
 }
